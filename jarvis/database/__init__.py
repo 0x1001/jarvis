99784be73c5049ac6786a9b2aa09ecc97f77dbda
Damian Nowok <damian.nowok@gmail.com>
2014-03-27 20:18:55 +0100

    Adding simple datebase interface

--- jarvis/database/__init__.py
+++ jarvis/database/__init__.py
@@ -1,3 +1,4 @@
 from word_parser import WordParser,WordParserException
+from database import WordDataBase, DataBaseException
 
 __all__ = ["WordParser","WordParserException"]
--- jarvis/test/test.py
+++ jarvis/test/test.py
@@ -101,5 +101,52 @@ class LowLevelTests(unittest.TestCase):
 
         os.unlink(self.__dummy_file)
 
+class WordDataBaseTest(unittest.TestCase):
+    def setUp(self):
+        from database import WordDataBase
+        self.db = WordDataBase()
+
+        self.db.addWord("ccc")
+        self.db.addWord("bbb")
+        self.db.addWord("aaa")
+
+    def test_wordId_invalidword(self):
+        from database import DataBaseException
+
+        with self.assertRaises(DataBaseException):
+            self.db.wordId(None)
+
+        with self.assertRaises(DataBaseException):
+            self.db.wordId("ddd")
+
+    def test_wordId_validword(self):
+        from database import DataBaseException
+
+        self.assertEqual(self.db.wordId("ccc"),0)
+        self.assertEqual(self.db.wordId("bbb"),1)
+        self.assertEqual(self.db.wordId("aaa"),2)
+
+    def test_addWord_invalidword(self):
+        from database import DataBaseException
+
+        with self.assertRaises(DataBaseException):
+            self.db.addWord(None)
+
+        with self.assertRaises(DataBaseException):
+            self.db.addWord("ccc")
+
+    def test_idWord_invalidinput(self):
+        from database import DataBaseException
+
+        with self.assertRaises(DataBaseException):
+            self.db.idWord(None)
+
+        with self.assertRaises(DataBaseException):
+            self.db.idWord(3)
+
+        with self.assertRaises(DataBaseException):
+            self.db.idWord(-5)
+
+
 if __name__ == "__main__":
     unittest.main()
