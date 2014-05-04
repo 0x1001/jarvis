35bb9eb54384bea60d55e4740baf28ffa4a05700
Damian Nowok <damian.nowok@gmail.com>
2014-05-04 10:58:54 +0200

    Hello ability

--- jarvis/abilities/a_hello.py
+++ jarvis/abilities/a_hello.py
@@ -16,11 +16,11 @@ class Hello(Ability):
 
         current = datetime.datetime.now()
 
-        if current.hour > 4 and current.hour < 12:
+        if current.hour >= 4 and current.hour < 12:
             response = [WordRecord("good"),WordRecord("morning")]
-        elif current.hour > 12 and current.hour < 18:
+        elif current.hour >= 12 and current.hour < 18:
             response = [WordRecord("good"),WordRecord("afternoon")]
-        elif ( current.hour > 18 and current.hour < 24 ) or current.hour < 4 :
-            response = [WordRecord("good"),WordRecord("afternoon")]
+        elif ( current.hour >= 18 and current.hour <= 24 ) or current.hour < 4 :
+            response = [WordRecord("good"),WordRecord("evening")]
 
         return response
--- jarvis/test/test.py
+++ jarvis/test/test.py
@@ -4,24 +4,24 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-##from braintest import *
-##from trainertest import *
-##from trainingdatabasetest import *
-##from worddatabasetest import *
-##from wordparsertest import *
-##from lowleveltest import *
-##from jarvistest import *
-##from worddatabasebuildertest import *
-##from trainingdatabasebuildertest import *
-##from databasebuildertest import *
-##from neuraltrainertest import *
-##from abilitiesdatabasetest import *
-##from abilitiesdatabasebuilderstest import *
+from braintest import *
+from trainertest import *
+from trainingdatabasetest import *
+from worddatabasetest import *
+from wordparsertest import *
+from lowleveltest import *
+from jarvistest import *
+from worddatabasebuildertest import *
+from trainingdatabasebuildertest import *
+from databasebuildertest import *
+from neuraltrainertest import *
+from abilitiesdatabasetest import *
+from abilitiesdatabasebuilderstest import *
 from abilitytest import *
 from bodytest import *
-##from recordtest import *
-##from voicetest import *
-##from consoletest import *
+from recordtest import *
+from voicetest import *
+from consoletest import *
 
 if __name__ == "__main__":
     unittest.main()
